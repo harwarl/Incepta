3abba0f8c28bd39333b6c9fcbcbeb471a430067b
harwarl <harwarl87@gmail.com>
2025-01-26 23:36:52 +0100

    feat: mentions

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.post(
   "/interactions",
   verifyKeyMiddleware(process.env.PUBLIC_KEY),
   async (req, res) => {
-    const { type, id, data } = req.body;
+    const { type, id, data, user } = req.body;
 
     if (type === InteractionType.PING) {
       return res.send({ type: InteractionResponseType.PONG });
@@ -32,15 +32,27 @@ app.post(
         return res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
-            content: "Hello There 👽",
+            content: `Hello There <@${user.id}> 👽`,
           },
         });
       }
+
+      //If the command does not exist
+      console.error(`unknown command: ${name}`);
+      return res.status(400).json({ error: "unknown command" });
     }
+
+    //For Unknown interaction type
+    console.error("unknown interaction type", type);
+    return res.status(400).json({ error: "unknown interaction type" });
   }
 );
 
-app.post("/webhook", async (req, res) => {});
+app.post("/webhook", async (req, res) => {
+  const { api_key } = req.query;
+  console.log({ api_key });
+  return res.send({ api_key });
+});
 
 app.listen(PORT, () => {
   console.log(`Bot is running on http://localhost:${PORT}`);
