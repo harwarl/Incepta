7d8644f6ecf1ba7480a0a43049ab53e5838a0c8a
harwarl <harwarl87@gmail.com>
2025-01-29 15:50:10 +0100

    update: commands and options

--- choices.js
+++ choices.js
@@ -1,4 +1,4 @@
-const setUpChoices = {
+export const setUpChoices = {
   "🔑 API_KEY": {
     description: "Alphabot API KEY",
   },
@@ -24,11 +24,66 @@ const setUpChoices = {
   },
 };
 
-export function getSetUpChoices() {
-  return Object.keys(setUpChoices);
+export const date_filterChoices = {
+  "Last 24 hours": "24 h",
+  "last 7 days": "7 d",
+  "All Time": "all",
+};
+
+export const sort_byChoices = {
+  "Latest win": "latest",
+  "Minting soon": "soon",
+};
+
+export const api_keyChoices = {
+  "set api key": "set",
+  remove: "remove",
+};
+
+export const whiteListActions = {
+  "Add to Whitelist": "add",
+  "Remove to Whitelist": "remove",
+};
+
+export const blackListActions = {
+  "Add to Blacklist": "add",
+  "Remove to Blacklist": "remove",
+};
+
+export const trendingTimeFilter = {
+  "1 week": "",
+  "2 week": "",
+  "1 month": "",
+};
+
+export const trendingStatus = {
+  "Active Raffles": "active",
+  All: "all",
+};
+
+export const pendingTimeFilter = {
+  "Ending in 1 hour": "1 hr",
+  "Ending in 1 day": "1 day",
+  All: "all",
+};
+
+export const pendingSortBy = {
+  Newest: "newest",
+  "End Soon": "soon",
+};
+
+export const removeAll = {
+  true: "true",
+  false: "false",
+};
+
+//Get CHoices keys
+export function getChoicesKeys(object) {
+  return Object.keys(object);
 }
 
-export function createCommandChoices(choices) {
+export function createCommandChoices(choicesObj) {
+  const choices = getChoicesKeys(choicesObj);
   const commandChoices = [];
 
   for (let choice of choices) {
--- commands.js
+++ commands.js
@@ -1,8 +1,31 @@
 import { COMMANDS } from "./constants.js";
 import { InstallGlobalCommands } from "./utils.js";
-import { createCommandChoices, getSetUpChoices } from "./choices.js";
+import {
+  api_keyChoices,
+  blackListActions,
+  createCommandChoices,
+  date_filterChoices,
+  pendingSortBy,
+  pendingTimeFilter,
+  removeAll,
+  setUpChoices,
+  sort_byChoices,
+  trendingStatus,
+  trendingTimeFilter,
+  whiteListActions,
+} from "./choices.js";
 
-const setupCommandChoices = createCommandChoices(getSetUpChoices());
+const setupCommandChoices = createCommandChoices(setUpChoices);
+const dateFilterCHoices = createCommandChoices(date_filterChoices);
+const sortByChoices = createCommandChoices(sort_byChoices);
+const setApiKeyChoices = createCommandChoices(api_keyChoices);
+const whiteListChoices = createCommandChoices(whiteListActions);
+const blackListChoices = createCommandChoices(blackListActions);
+const trendingTimeChoices = createCommandChoices(trendingTimeFilter);
+const trendingStatusChoices = createCommandChoices(trendingStatus);
+const pendingTimeChoices = createCommandChoices(pendingTimeFilter);
+const pendingSort = createCommandChoices(pendingSortBy);
+const removeALL = createCommandChoices(removeAll);
 
 //Basic Test Command
 const TEST_COMMAND = {
@@ -59,6 +82,7 @@ const API_KEY_COMMAND = {
   name: COMMANDS.API_KEY,
   type: 1,
   description: "Manage your api key (check, set or remove)",
+  choices: setApiKeyChoices,
   integration_types: [0, 1],
   contexts: [1, 2],
 };
@@ -67,6 +91,22 @@ const RAFFLE_WINS_COMMAND = {
   name: COMMANDS.RAFFLE_WINS,
   type: 1,
   description: "Check your recent raffle wins",
+  options: [
+    {
+      type: 3,
+      name: "date_filter",
+      description: "Select the time frame for filtering wins",
+      required: true,
+      choices: dateFilterCHoices,
+    },
+    {
+      type: 3,
+      name: "sort_by",
+      required: true,
+      description: "...",
+      choices: sortByChoices,
+    },
+  ],
   integration_types: [0, 1],
   contexts: [1, 2],
 };
@@ -75,6 +115,22 @@ const PENDING_COMMAND = {
   name: COMMANDS.PENDING,
   type: 1,
   description: "Check your recent raffle registration",
+  options: [
+    {
+      type: 3,
+      name: "time_filter",
+      description: "Select the time frame for filtering raffles",
+      required: true,
+      choices: pendingTimeChoices,
+    },
+    {
+      type: 3,
+      name: "sort_by",
+      required: true,
+      description: "...",
+      choices: pendingSort,
+    },
+  ],
   integration_types: [0, 1],
   contexts: [1, 2],
 };
@@ -83,6 +139,22 @@ const TRENDING_COMMAND = {
   name: COMMANDS.TRENDING,
   type: 1,
   description: "Check trending projects",
+  options: [
+    {
+      type: 3,
+      name: "time_filter",
+      description: "Select the time frame for trending projects",
+      required: true,
+      choices: trendingTimeChoices,
+    },
+    {
+      type: 3,
+      name: "status",
+      description: "choose whether to show only active raffles or all raffles",
+      required: true,
+      choices: trendingStatusChoices,
+    },
+  ],
   integration_types: [0, 1],
   contexts: [1, 2],
 };
@@ -99,6 +171,16 @@ const WHITELIST_LIST_COMMAND = {
   name: COMMANDS.WHITELIST_LIST,
   type: 1,
   description: "View or remove all your whitelisted projects and communities",
+  options: [
+    {
+      type: 3,
+      name: "remove_all",
+      description:
+        "Set to True to remove all whitelisted projects and communities",
+      required: false,
+      choices: removeALL,
+    },
+  ],
   integration_types: [0, 1],
   contexts: [1, 2],
 };
@@ -107,6 +189,21 @@ const WHITELIST_COMMAND = {
   name: COMMANDS.WHITELIST,
   type: 1,
   description: "Manage your raffle whitelist (project or communities)",
+  options: [
+    {
+      name: "item",
+      type: 3,
+      description: "List of twitter handles",
+      required: true,
+    },
+    {
+      name: "action",
+      type: 3,
+      description: "Add or remove project(s) or communities from the whitelist",
+      required: true,
+      choices: whiteListChoices,
+    },
+  ],
   integration_types: [0, 1],
   contexts: [1, 2],
 };
@@ -123,6 +220,21 @@ const BLACKLIST_COMMAND = {
   name: COMMANDS.BLACKLIST,
   type: 1,
   description: "Manage your raffle blacklist (project or communities)",
+  options: [
+    {
+      name: "item",
+      type: 3,
+      description: "List of twitter handles",
+      required: true,
+    },
+    {
+      name: "action",
+      type: 3,
+      description: "Add or remove project(s) or communities from the whitelist",
+      required: true,
+      choices: blackListChoices,
+    },
+  ],
   integration_types: [0, 1],
   contexts: [1, 2],
 };
