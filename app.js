2660c588053f14abe57f6897c879b7ccaf5a729e
harwarl <harwarl87@gmail.com>
2025-02-02 23:07:07 +0100

    feat: well

--- app.js
+++ app.js
@@ -183,11 +183,11 @@ app.post(
       //   });
       // }
 
-      if (name === COMMANDS.PENDING && id) {
-        return res.send({
-          type: InteractionResponseType.PONG,
-        });
-      }
+      // if (name === COMMANDS.PENDING && id) {
+      //   return res.send({
+      //     type: InteractionResponseType.PONG,
+      //   });
+      // }
 
       if (name === COMMANDS.RAFFLE_WINS && id) {
         return res.send({
